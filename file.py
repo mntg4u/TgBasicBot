cd79a074979ea399a94cb88bdde757337584faf5
Dev Krishna Ajith <130361057+CpFlicks@users.noreply.github.com>
2024-11-12 15:29:16 +0530

    Update file.py

--- file.py
+++ file.py
@@ -2,7 +2,7 @@ from pyrogram import Client
 
 api_id = "13666216"
 api_hash = "f3a456b486290011638fb4b312f9be70"
-session_string = "" 
+session_string = "BQDQh6gAxWdEbgLaPn69d_1-z31QO2hIZAoCPc6mnRkeQ-4EqKLNCSMH7DcXUyzcBEqKI8sE0xMcsmSEhEptJ3lcfhNHsnniO0pnd75EYfyYIcNtKaveAl0g62jknqVqrmYrwJZjuSrWJb8WzJ7fT7LvlJq_JRRp1hDasgFikNJshtz8MEtiWMyVc0nQr4iJ7bGbmkRThrG4NW70grXK38d54O7Dg8713Wc39zFLuZhrEX2VKThAUm-HqucFgSHxvmXk92Nc_W3mtpMHbfyV32p32-Fp4dKVk4YlU2VL6Fnihz3rEPcS8F9r4Z89RSLp1zLHZHrzwKRqCVvx2tWl9KhwMJWQ_QAAAAHUxLFoAA" 
 
 app = Client("my_account", api_id=api_id, api_hash=api_hash, session_string=session_string)
 
