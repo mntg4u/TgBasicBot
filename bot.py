ec245b6025455c50278e1b98757858cc6083b4bc
Dev Krishna Ajith <130361057+CpFlicks@users.noreply.github.com>
2024-11-19 19:14:14 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,45 @@
+#𝘼𝙗𝙊𝙪𝙩𝙈𝙚_𝘿𝙆 ⚡
+
+from aiohttp import web
+from route import routes
+from plugins import web_server
+
+import pyromod.listen
+from pyrogram import Client
+from pyrogram.enums import ParseMode
+import sys
+from datetime import datetime
+
+from config import API_HASH, APP_ID, LOGGER, TG_BOT_WORKERS, PORT, TG_BOT_TOKEN
+    
+class Bot(Client):
+    def __init__(self):
+        super().__init__(
+            name="Bot",
+            api_hash=API_HASH,
+            api_id=APP_ID,
+            bot_token=TG_BOT_TOKEN,
+            plugins={
+                "root": "plugins"
+            },
+            workers=TG_BOT_WORKERS
+        )
+        self.LOGGER = LOGGER
+
+    async def start(self):
+        await super().start()
+        print("Bot is AcTiVe!")
+        usr_bot_me = await self.get_me()
+        self.uptime = datetime.now()
+        self.set_parse_mode(ParseMode.HTML)
+        self.LOGGER(__name__).info(f"Bot is AcTiVe!")
+       
+        #web-response
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()
+
+    async def stop(self, *args):
+        await super().stop()
+        self.LOGGER(__name__).info("Bot stopped.")
