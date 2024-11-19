03e399b01c617426d49e6ad84532d110f23ef479
Dev Krishna Ajith <130361057+CpFlicks@users.noreply.github.com>
2024-11-19 21:12:12 +0530

    Update studocu.py

--- plugins/studocu.py
+++ plugins/studocu.py
@@ -1,6 +1,7 @@
 import aiohttp
 import asyncio
 import os
+from bot import Bot
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
