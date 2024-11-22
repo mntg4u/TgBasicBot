b01d52f2616a6903dd75c2a1d1ab58ddc616d3d6
Dᴋ 🇮🇳 <2392devkrishnaajith@gmail.com>
2024-11-22 17:01:42 +0530

    Done ✅

--- config.py
+++ config.py
@@ -12,16 +12,17 @@ APP_ID = int(os.environ.get("APP_ID", "13666216"))
 #Your API Hash from my.telegram.org
 API_HASH = os.environ.get("API_HASH", "f3a456b486290011638fb4b312f9be70")
 
-#OWNER ID
+#Your Telegram Bot's API Token from @BotFather
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7572076027:AAG2_9cz5jItaszpa7RgmiQQOVVCe78e94M")
+
+#Bot Owner's Telegram ID 
 OWNER_ID = int(os.environ.get("OWNER_ID", "5465110453"))
 
-#Port
+#No Need to Change This 
 PORT = os.environ.get("PORT", "8080")
 
+#No need to Change This 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
-
-#bot token
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7572076027:AAG2_9cz5jItaszpa7RgmiQQOVVCe78e94M")
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
