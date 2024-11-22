9e7fdc2f80bb5f4dd670ebd4012d2bafc859937d
Dᴋ 🇮🇳 <2392devkrishnaajith@gmail.com>
2024-11-22 21:49:03 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,10 +1,10 @@
 
 from pyrogram import __version__
 
-ABOUT_TXT = f"""<b><i>@tgBaSiCbOt is an Open Source project made by <a href='https://t.me/DevKrishnaAjith'>Dev Krishna Ajith</a> to make Telegram bot development easier for beginners.\n\nIf you are interested to build a Telegram Bot, this project can help you to build it easily because it's a Simple Repository which you can easily understand and it's simple to modify.</b></i>
+USER_START_TXT = f"""<b><i>@tgBaSiCbOt is an Open Source project made by <a href='https://t.me/DevKrishnaAjith'>Dev Krishna Ajith</a> to make Telegram bot development easier for beginners.\n\nIf you are interested to build a Telegram Bot, this project can help you to build it easily because it's a Simple Repository which you can easily understand and it's simple to modify.</b></i>
 """
 
-START_TXT = f"""<i><b>🎊 Congratulations, You successfully builded a Telegram Bot!! 🥳</i></b>
+OWNER_START_TXT = f"""<i><b>🎊 Congratulations, You successfully builded a Telegram Bot!! 🥳</i></b>
 
 <i>Are you ready to turn your brilliant bot idea into reality? You’ve taken the first step by reading this! Now, let’s turn that vision into a tangible, powerful tool.</i>
 
