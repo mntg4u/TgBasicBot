1d42ae854ccfcb86df0943ad753f1fd8b4e3466f
Dᴋ 🇮🇳 <2392devkrishnaajith@gmail.com>
2024-11-22 22:54:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,16 +7,16 @@ from logging.handlers import RotatingFileHandler
 from time import time
 
 #Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", "13666216"))
+API_ID = int(os.environ.get("API_ID", "PLACE_YOUR_TELEGRAM_API_ID"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "f3a456b486290011638fb4b312f9be70")
+API_HASH = os.environ.get("API_HASH", "PLACE_YOUR_TELEGRAM_API_HASH")
 
 #Your Telegram Bot's API Token from @BotFather
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7866637058:AAEKDTC7s6YphuUns2dMEkvvAvGXycbfuHI")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "PLACE_YOUR_TELEGRAM_BOT_API_TOKEN")
 
 #Bot Owner's Telegram ID 
-OWNER_ID = int(os.environ.get("OWNER_ID", "6691182031"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "PLACE_YOUR_TELEGRAM_USER_ID"))
 
 #No Need to Change This 
 PORT = os.environ.get("PORT", "8080")
