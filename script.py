e9714865a0bd55929877a2be74cd35f06b8e44b7
Dᴋ 🇮🇳 <2392devkrishnaajith@gmail.com>
2024-11-22 20:49:18 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,7 @@
 
+from pyrogram import __version__
+
+ABOUT_MESSAGE = f"""<b><i>@tgBaSiCbOt is an Open Source project made by <a href='https://t.me/DevKrishnaAjith'>Dev Krishna Ajith</a> to make Telegram bot development easier for beginners.\n\nIf you are interested to build a Telegram Bot, this project can help you to build it easily because it's a Simple Repository which you can easily understand and it's simple to modify.</b></i>
+
+<i><b>An @ExamVault Project</i></b>
+"""
