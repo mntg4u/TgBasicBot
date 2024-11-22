9e3e0c0f7cf35a290cb224848b67914756714715
Dᴋ 🇮🇳 <2392devkrishnaajith@gmail.com>
2024-11-22 21:46:10 +0530

    Done ✅

---
+++ plugins/callbacks.py
@@ -0,0 +1,27 @@
+import os
+import re
+import asyncio
+from pyrogram import Client, enums, filters, __version__
+from pyrogram.enums import ParseMode
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from bot import Bot
+from config import OWNER_ID
+from script import NEXT_TXT
+
+contact_btn = [
+    [
+        InlineKeyboardButton(text = 'Reach out to him', url = 'https://t.me/DevKrishnaAjith')
+    ]
+]
+
+@Bot.on_callback_query(filters.regex(r'^next$'))
+async def next_cbq(client: Client, query: CallbackQuery):
+    await query.message.chat.send_message(
+        text = NEXT_TXT,
+        reply_markup = InlineKeyboardMarkup(contact_btn),
+        disable_web_page_preview = True,
+        parse_mode=enums.ParseMode.HTML
+    )
+     
+        
+   
