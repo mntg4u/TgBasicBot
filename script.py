9c3aff92e232044f73fe43fa4b39dab003e0ad03
Dᴋ 🇮🇳 <2392devkrishnaajith@gmail.com>
2024-11-24 02:52:10 +0530

    Done ✅

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 
 from pyrogram import __version__
 
-USER_START_TXT = f"""<b><i><a href='https://github.com/AbOutMeDK/tgBaSiCbOt'>@tgBaSiCbOt</a></b> is an Open Source project made by <a href='https://t.me/DevKrishnaAjith'>Dev Krishna Ajith</a> to make Telegram bot development easier for beginners.\n\nIf you are interested to build a Telegram Bot, this project can help you to build it easily because it's a Simple Repository which you can easily understand and it's simple to modify.</b></i>
+ABOUT_TXT = f"""<b><i><a href='https://github.com/AbOutMeDK/tgBaSiCbOt'>@tgBaSiCbOt</a></b> is an Open Source project made by <a href='https://t.me/DevKrishnaAjith'>Dev Krishna Ajith</a> to make Telegram bot development easier for beginners.\n\nIf you are interested to build a Telegram Bot, this project can help you to build it easily because it's a Simple Repository which you can easily understand and it's simple to modify.</b></i>
 """
 
 OWNER_START_TXT = f"""<i><b>🎊 Congratulations, You successfully builded a Telegram Bot!! 🥳</i></b>
@@ -18,4 +18,20 @@ NEXT_TXT = f"""<b><i>Now, please forward the initial message received from the b
 <i><b>By forwarding the message you are sending verification request to @tgBaSiCbOt moderators. After reviewing your request they'll connect with you for further update as soon as possible</i></b>
 
 <i><b>Thank You,\nTeam @ExamVault</i></b>
+"""
+
+USER_START_TXT = f"""<b><i>Want to build your own Telegram bot? Let's get started!</b></i>
+
+<b><i><a href='https://github.com/AbOutMeDK/tgBaSiCbOt'>@tgBaSiCbOt</a> is a Project initiated by Team <a href='https://t.me/examVault'>Exam Vault</a> to provide a comprehensive guide for those starting their journey in Telegram Bot development.</i></b>
+
+<b><i>This project offers a simplified Python repository to facilitate the development of a BaSiC Telegram bot. It provides a clear and accessible foundation for beginners and experienced developers alike.</b></i>
+
+<b><i>Here is the source code of our BaSiC Telegram Bot which you can use to build your First Bot:</i></b>
+
+<i>• https://github.com/AbOutMeDK/tgBaSiCbOt</i>
+
+<b><i>We hope this project will help those interested in building Telegram bots but don't know where to start. We're here to guide you.</b></i>
+
+<b><i>Thank You,</b></i>
+<b><i>Team @ExamVault</b></i>
 """
